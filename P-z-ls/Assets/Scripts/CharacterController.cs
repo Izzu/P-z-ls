2441b276e56bec9f2a57400784dcb60b3d9c3010
Andrew <colvinwandrew@gmail.com>
2016-04-03 23:34:24 -0500

    Added Character, Working on Movement

---
+++ P-z-ls/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(moveH, 0, moveV);
+        rigid.AddForce(move * speed);
+    }
+}
