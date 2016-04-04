cc7736165f8b693177b057eb0a294056f4e45413
Andrew <colvinwandrew@gmail.com>
2016-04-04 01:29:31 -0500

    Movement, Mouse Look, Highlighting

---
+++ P-z-ls/Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    public float speed = 6f;
+    public float rotateSpeed = 0.0005f;
+    Vector3 movement;
+    Vector3 rotation;
+    Rigidbody playerRigidbody;
+
+    // Use this for initialization
+    void Awake () {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //float rotate = Input.GetAxis("Horizontal");
+        float move = Input.GetAxisRaw("Vertical");
+        Move(move);
+        //Turning(rotate);
+    }
+
+    void Move(float move)
+    {
+        movement.Set(0f, 0f, move);
+        movement = movement.normalized * speed * Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        playerRigidbody.MovePosition(playerRigidbody.position + movement);
+    }
+
+    /*void Turning(float rotate)
+    {
+        rotation.Set(0f, rotate, 0f);
+        rotation = rotation.normalized * rotateSpeed;
+        Quaternion rotated = Quaternion.Euler(rotation);
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * rotated);
+
+    }*/
+}
---
+++ P-z-ls/Assets/Scripts/Highlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highlight : MonoBehaviour {
+
+    private Color startcolor;
+
+    void OnMouseEnter()
+
+    {
+        //startcolor = renderer.material.color;
+        startcolor = GetComponent<Renderer>().material.color;
+        //renderer.material.color = Color.red;
+        GetComponent<Renderer>().material.color = new Color(0.09f, 0.75f, 0.75f, 0.75f);
+    }
+    void OnMouseExit()
+    {
+        //renderer.material.color = startcolor;
+        GetComponent<Renderer>().material.color = startcolor;
+    }
+}
