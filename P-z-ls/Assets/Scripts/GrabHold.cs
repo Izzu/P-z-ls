812bf5b9ae5d1b33de1b18eea9980115654e1d6c
Andrew <colvinwandrew@gmail.com>
2016-04-18 09:51:21 -0500

    Puzzle 2

---
+++ P-z-ls/Assets/Scripts/GrabHold.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabHold : MonoBehaviour {
+    GameObject grabbedObject;
+    float grabbedObjectSize;
+    public float range;
+
+    GameObject GetMouseHoverObject(float range)
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit raycastHit;
+        Vector3 target = position + Camera.main.transform.forward;
+
+        if(Physics.Linecast(position, target, out raycastHit))
+        {
+            return raycastHit.collider.gameObject;
+        }
+
+        return null;
+    }
+	
+    void TryGrabObject(GameObject grabObject)
+    {
+        if (grabObject == null || !CanGrab(grabObject))
+            return;
+
+        grabbedObject = grabObject;
+        grabbedObjectSize = grabbedObject.GetComponent<Renderer>().bounds.size.magnitude;
+    }
+
+    bool CanGrab(GameObject anObject)
+    {
+        return anObject.GetComponent<Rigidbody>() != null;
+    }
+
+    void DropObject()
+    {
+        if (grabbedObject == null)
+            return;
+
+        if (grabbedObject.GetComponent<Rigidbody>() != null)
+            grabbedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        grabbedObject = null;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(grabbedObject == null)
+            {
+                TryGrabObject(GetMouseHoverObject(range));
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+        if(grabbedObject != null)
+        {
+            Vector3 newPosition = gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize;
+            grabbedObject.transform.position = newPosition;
+        }
+	}
+}
