7c77d79300b25b3b48ce98aefad69af0c85097f0
Andrew <colvinwandrew@gmail.com>
2016-04-27 11:09:08 -0500

    1st and 2nd Puzzle Done

--- P-z-ls/Assets/Scripts/Event1.cs
+++ P-z-ls/Assets/Scripts/Event1.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Event1 : MonoBehaviour {
 
+    private int count;
+    public GameObject particles;
+
 	// Use this for initialization
 	void Start () {
-	
+        count = 0;
+        particles.SetActive(false);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void OnTriggerEnter(Collider other)
+    {
+        count++;
+        Debug.Log(count);
+
+        if(count == 5)
+        {
+            particles.SetActive(true);
+        }
+    }
 }
